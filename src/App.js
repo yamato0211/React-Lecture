b617ea2b86682d4aeafbd656873628babc6711db
yamato0211 <kumayama0211@gmail.com>
2023-02-19 22:30:12 +0900

    install axios and fetch dog image feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import TodoList from './Components/TodoList';
+import axios from "axios"
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("")
+  useEffect(() => {
+    async function getDogImageUrl() {
+      const response = await axios.get("https://dog.ceo/api/breeds/image/random")
+      console.log(response)
+      setImageUrl(response.data.message)
+    }
+    getDogImageUrl()
+  },[]) 
   return (
     <div className="App">
+      <img src={imageUrl} alt="" />
       <TodoList />
     </div>
   );
