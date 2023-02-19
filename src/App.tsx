babeac4718e05ad7766baae0b0f1625402f04fba
yamato0211 <kumayama0211@gmail.com>
2023-02-19 22:36:29 +0900

    install axios and fecth dog image feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import TodoList from './Components/TodoList';
+import axios from "axios"
+
+type ResponseType = {
+  message: string
+  status: string
+}
 
 function App() {
+  const [imageUrl, setImageUrl] = useState<ResponseType>()
+  useEffect(() => {
+    async function getDogImageUrl() {
+      const response = await axios.get("https://dog.ceo/api/breeds/image/random") 
+      setImageUrl(response.data)
+    }
+    getDogImageUrl()
+  },[])
   return (
     <div className="App">
+      <img src={imageUrl?.message} alt=""/>
       <TodoList />
     </div>
   );
